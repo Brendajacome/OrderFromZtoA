ae29fed8670eb4cd686ea6f3ea24b3990d1659ef
Brendajacome <bcjacomea@uce.edu.ec>
2024-06-05 00:18:23 -0500

    Update HelloApplication.java

--- src/main/java/com/example/orderfromztoa/HelloApplication.java
+++ src/main/java/com/example/orderfromztoa/HelloApplication.java
@@ -1,25 +1,23 @@
 package com.example.orderfromztoa;
-import java.util.Arrays;
 
 public class HelloApplication {
 
     public static void main(String[] args) {
-        String[] letters = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
-        System.out.println("Order from  Z to A: ");
-        order(letters, 0, letters.length - 1);
-        System.out.println(Arrays.toString(letters));
-    }
-
-    private static void order(String[] letters, int start, int end) {
-        if (start < end) {
-            for (int i = start; i < end; i++) {
-                if (letters[i].compareTo(letters[i + 1]) < 0) {
-                    String temp = letters[i];
-                    letters[i] = letters[i + 1];
-                    letters[i + 1] = temp;
+        char[] letters  = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        int order = letters .length;
+        for (int i = 0; i < order - 1; i++) {
+            for (int j = 0; j < order - 1 - i; j++) {
+                if (letters [j] < letters [j + 1]) {
+                    char temp = letters [j];
+                    letters [j] = letters [j + 1];
+                    letters [j + 1] = temp;
                 }
             }
-            order(letters, start, end - 1);
+        }
+
+        System.out.println("The letters ordered from Z to A are:");
+        for (int i = 0; i < order; i++) {
+            System.out.print(letters [i] + " ");
         }
     }
 }
